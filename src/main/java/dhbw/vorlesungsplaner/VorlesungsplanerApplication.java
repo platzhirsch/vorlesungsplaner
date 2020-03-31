ae6f5afe6fee60375266f610c922916440f91480
Felix Flassak <felixflassak@gmail.com>
2020-03-31 14:38:40 +0200

    initial commit

---
+++ src/main/java/dhbw/vorlesungsplaner/VorlesungsplanerApplication.java
@@ -0,0 +1,25 @@
+package dhbw.vorlesungsplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class VorlesungsplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorlesungsplanerApplication.class, args);
+	}
+
+}
+
+@RestController
+class HellController {
+
+	@GetMapping("/")
+		String hello(){
+		return "Hello";
+		}
+}
---
+++ src/test/java/dhbw/vorlesungsplaner/VorlesungsplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.vorlesungsplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorlesungsplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
