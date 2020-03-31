f55ddae856d59a90b769e64fe731c8b68bf7679b
Felix Flassak <felixflassak@gmail.com>
2020-03-31 18:34:36 +0200

    jany

--- src/main/java/dhbw/vorlesungsplaner/security/JwtUserDetailsService.java
+++ src/main/java/dhbw/vorlesungsplaner/security/JwtUserDetailsService.java
@@ -13,8 +13,8 @@ public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("admin".equals(username)) {
-            return new User("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+        if ("Jany".equals(username)) {
+            return new User("Jany", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
                     new ArrayList<>());
             //
         } else {
