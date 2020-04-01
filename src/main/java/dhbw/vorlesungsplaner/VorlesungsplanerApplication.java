11edcb02182acb2b281bdd9fd3e60a70f94dc84a
Felix Flassak <felixflassak@gmail.com>
2020-04-01 18:57:41 +0200

    Max users db connection

--- src/main/java/dhbw/vorlesungsplaner/VorlesungsplanerApplication.java
+++ src/main/java/dhbw/vorlesungsplaner/VorlesungsplanerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(ApplicationProperties.class)
 public class VorlesungsplanerApplication {
 
 	public static void main(String[] args) {
